5629fa34a4997405361e028e3e60c66f9cd6eb01
ms4985 <ms4985@columbia.edu>
2016-04-28 20:38:00 -0400

    changed timing implementation- to test on clic

--- part2/main.c
+++ part2/main.c
@@ -20,6 +20,8 @@ int main(int argc, char* argv[]) {
                 assert(fanout[i] >= 2 && fanout[i] <= 17);
         }
 
+        p1_end = p2_begin = clock();
+
         // building the tree index
         rand32_t* gen = rand32_init((uint32_t) time(NULL));
         assert(gen != NULL);
@@ -40,15 +42,27 @@ int main(int argc, char* argv[]) {
         uint32_t* result = malloc(sizeof(uint32_t) * num_probes);
         assert(result != NULL);
 
+        p1_end = p2_begin = clock();
+
         // perform index probing (Phase 2)
         for (size_t i = 0; i < num_probes; ++i) {
                 result[i] = probe_index(tree, probe[i]);
         }
 
+        p2_end = p3_begin = clock();
+
         // output results
         for (size_t i = 0; i < num_probes; ++i) {
                 fprintf(stdout, "%d %u\n", probe[i], result[i]);
         }
+
+        p3_end = clock();
+
+        printf("Phase 1: %f s\n",(double)(p1_end - p1_begin) / CLOCKS_PER_SEC);
+
+        printf("Phase 2: %f s\n",(double)(p2_end - p2_begin) / CLOCKS_PER_SEC);
+
+        printf("Phase 3: %f s\n",(double)(p3_end - p3_begin) / CLOCKS_PER_SEC);
 
         // cleanup and exit
         free(result);
